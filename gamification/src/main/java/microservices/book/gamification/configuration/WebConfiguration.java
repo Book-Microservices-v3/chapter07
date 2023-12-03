df214624960f8c8416e46d878eaa4be2e8b1824a
Tarun Telang <tarun.telang@gmail.com>
2023-12-03 09:49:43 +0530

    Update WebConfiguration.java

--- gamification/src/main/java/microservices/book/gamification/configuration/WebConfiguration.java
+++ gamification/src/main/java/microservices/book/gamification/configuration/WebConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     * More info: https://docs.spring.io/spring-framework/reference/web/webmvc-cors.html
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:3000");
